d364f185ae09080890af80e8714ed88ef6f9e75d
Marwan Tarek <119248877+Marwannnnnntarek@users.noreply.github.com>
2025-06-04 18:10:06 +0300

    solve firebase problems

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -23,8 +23,8 @@ dependencies {
 
 android {
     namespace = "com.example.todo"
-    compileSdk = flutter.compileSdkVersion
-     ndkVersion = "27.0.12077973"
+    compileSdk = 34
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
@@ -40,8 +40,8 @@ android {
         applicationId = "com.example.todo"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
+        minSdk = 23
+        targetSdk = 34
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 // ignore: deprecated_member_use, library_prefixes
-import 'package:firebase/firebase.dart' as Firebase;
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive_flutter/hive_flutter.dart';
@@ -12,8 +12,7 @@ Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   try {
-    WidgetsFlutterBinding.ensureInitialized();
-    Firebase.initializeApp();
+    await Firebase.initializeApp();
     await Hive.initFlutter();
     Hive.registerAdapter(TaskModelAdapter());
     await Hive.openBox<TaskModel>('task_box');
